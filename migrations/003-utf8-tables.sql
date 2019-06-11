d5d73ab8d65d11b8cdcf23f5d61b6398d4a8113c
Gregor Aisch <gka@users.noreply.github.com>
2019-06-11 10:55:11 +0200

    migrate pixeltracker tables to utf8mb4

---
+++ migrations/003-utf8-tables.sql
@@ -0,0 +1,12 @@
+-- Up
+ALTER TABLE pixeltracker_chart DEFAULT CHARSET=utf8mb4;
+ALTER TABLE pixeltracker_chart_embedurl DEFAULT CHARSET=utf8mb4;
+ALTER TABLE pixeltracker_domain_month DEFAULT CHARSET=utf8mb4;
+ALTER TABLE pixeltracker_organization_day DEFAULT CHARSET=utf8mb4;
+ALTER TABLE pixeltracker_organization_month DEFAULT CHARSET=utf8mb4;
+ALTER TABLE pixeltracker_organization_week DEFAULT CHARSET=utf8mb4;
+ALTER TABLE pixeltracker_user_day DEFAULT CHARSET=utf8mb4;
+ALTER TABLE pixeltracker_user_month DEFAULT CHARSET=utf8mb4;
+ALTER TABLE pixeltracker_user_week DEFAULT CHARSET=utf8mb4;
+
+-- Down
