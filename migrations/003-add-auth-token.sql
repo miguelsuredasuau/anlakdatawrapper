ff17e984a84fc24ec2af744f6c3ac155a2cabe7b
David Kokkelink <david@datawrapper.de>
2020-03-05 09:50:28 +0100

    dont hardcode auth_token id

--- migrations/003-add-auth-token.sql
+++ migrations/003-add-auth-token.sql
@@ -1,8 +1,8 @@
 -- Adds auth token to the existing user with id 1
 
 -- Up
-INSERT INTO auth_token (id,user_id,token,comment,created_at) VALUES
-(1,1,'e927d1b49deadf9186800d3759cdcf46de009fe705573dd67ab7cfcbf3efc153','default','2020-01-22 14:18:17.000');
+INSERT INTO auth_token (user_id,token,comment,created_at) VALUES
+(1,'e927d1b49deadf9186800d3759cdcf46de009fe705573dd67ab7cfcbf3efc153','default','2020-01-22 14:18:17.000');
 
 -- Down
 DELETE FROM auth_token WHERE id = 1;
