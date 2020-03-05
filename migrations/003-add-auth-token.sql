95912b4c20d905cd3e086610c5e326701c9ac3d5
David Kokkelink <david@datawrapper.de>
2020-03-05 09:53:53 +0100

    change down migration

--- migrations/003-add-auth-token.sql
+++ migrations/003-add-auth-token.sql
@@ -5,4 +5,4 @@ INSERT INTO auth_token (user_id,token,comment,created_at) VALUES
 (1,'e927d1b49deadf9186800d3759cdcf46de009fe705573dd67ab7cfcbf3efc153','default','2020-01-22 14:18:17.000');
 
 -- Down
-DELETE FROM auth_token WHERE id = 1;
+DELETE FROM auth_token WHERE token = 'e927d1b49deadf9186800d3759cdcf46de009fe705573dd67ab7cfcbf3efc153';
