efa45169ec7c387126aa7fbdf8cb2b3501ad963f
Gregor Aisch <gka@users.noreply.github.com>
2020-06-08 18:02:28 +0200

    move markerPresets out of map-styles

--- migrations/001-theme-refactor-map-styles.sql
+++ migrations/001-theme-refactor-map-styles.sql
@@ -12,6 +12,12 @@ UPDATE theme SET
 	`data` = JSON_REMOVE(JSON_SET(`data`, @newkey, JSON_EXTRACT(`data`, @oldkey)), @oldkey)
 	WHERE JSON_EXTRACT(`data`, @oldkey) IS NOT NULL;
 
+SET @oldkey = '$.vis."locator-maps".mapStyles[0].markerPresets';
+SET @newkey = '$.vis."locator-maps".markerPresets';
+UPDATE theme SET
+	`data` = JSON_REMOVE(JSON_SET(`data`, @newkey, JSON_EXTRACT(`data`, @oldkey)), @oldkey)
+	WHERE JSON_EXTRACT(`data`, @oldkey) IS NOT NULL;
+
 -- Down
 SET @oldkey = '$.vis."locator-maps".mapStyles';
 SET @newkey = '$."map-styles"';
@@ -21,6 +27,12 @@ UPDATE theme SET
 
 SET @oldkey = '$.vis."locator-maps".defaultMarker';
 SET @newkey = '$."map-styles"[0].defaultMarker';
+UPDATE theme SET
+	`data` = JSON_REMOVE(JSON_SET(`data`, @newkey, JSON_EXTRACT(`data`, @oldkey)), @oldkey)
+	WHERE JSON_EXTRACT(`data`, @oldkey) IS NOT NULL;
+
+SET @oldkey = '$.vis."locator-maps".markerPresets';
+SET @newkey = '$."map-styles"[0].markerPresets';
 UPDATE theme SET
 	`data` = JSON_REMOVE(JSON_SET(`data`, @newkey, JSON_EXTRACT(`data`, @oldkey)), @oldkey)
 	WHERE JSON_EXTRACT(`data`, @oldkey) IS NOT NULL;
