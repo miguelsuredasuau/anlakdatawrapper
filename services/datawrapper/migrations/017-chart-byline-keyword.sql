b12c280e07244fb474689b710dfddc0294d0e32a
Gregor Aisch <gka@users.noreply.github.com>
2021-09-07 10:52:12 +0000

    add describe.byline to chart search keywords (#16)
    * add describe.byline to chart search keywords
    * add LEFT(.., 4096)

--- services/datawrapper/migrations/017-chart-byline-keyword.sql
+++ services/datawrapper/migrations/017-chart-byline-keyword.sql
@@ -1,29 +1,27 @@
 -- Up
-ALTER TABLE chart ADD keywords TEXT NULL;
-CREATE FULLTEXT INDEX chart_fts_IDX ON chart (title, keywords);
+DROP TRIGGER update_keywords;
+DROP TRIGGER insert_keywords;
 
 CREATE TRIGGER insert_keywords BEFORE INSERT ON chart
-FOR EACH ROW SET NEW.keywords = LOWER(CONCAT(
+FOR EACH ROW SET NEW.keywords = LEFT(LOWER(CONCAT(
   COALESCE(NEW.metadata->>"$.describe.intro", ""), ". ",
+  COALESCE(NEW.metadata->>"$.describe.byline", ""), ". ",
   COALESCE(NEW.metadata->>"$.describe.\"source-name\"", ""),". ",
   COALESCE(NEW.metadata->>"$.describe.\"aria-description\"", ""), ". ",
   COALESCE(NEW.metadata->>"$.annotate.\"notes\"", ""), ". ",
   COALESCE(REPLACE(REPLACE(NEW.metadata->>"$.custom.*", '[', ' '), ']', ''), "")
-));
+)), 4096);
 
 CREATE TRIGGER update_keywords BEFORE UPDATE ON chart
-FOR EACH ROW SET NEW.keywords = LOWER(CONCAT(
+FOR EACH ROW SET NEW.keywords = LEFT(LOWER(CONCAT(
   COALESCE(NEW.metadata->>"$.describe.intro", ""), ". ",
+  COALESCE(NEW.metadata->>"$.describe.byline", ""), ". ",
   COALESCE(NEW.metadata->>"$.describe.\"source-name\"", ""), ". ",
   COALESCE(NEW.metadata->>"$.describe.\"aria-description\"", ""), ". ",
   COALESCE(NEW.metadata->>"$.annotate.\"notes\"", ""), ". ",
   COALESCE(REPLACE(REPLACE(NEW.metadata->>"$.custom.*", '[', ' '), ']', ''), "")
-));
+)), 4096);
 
 
 -- Down
-DROP TRIGGER update_keywords;
-DROP TRIGGER insert_keywords;
-ALTER TABLE chart DROP INDEX chart_fts_IDX;
-ALTER TABLE chart DROP COLUMN keywords;
 
