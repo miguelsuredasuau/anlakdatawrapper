6edc3ba6d807c24d5f99a27c1c7ad7eeeb8cfbe0
Marten Sigwart <marten@okamaja.de>
2021-10-08 18:23:15 +0200

    feat(datawrapper): show confirmation modal before changing team owner (#327)
    * display confirm alert before changing team member's role to owner
    
    * fix(datawrapper) refresh team members after changing a team member's role (#314)
    
    * fire updateUsers event after assigning new role to team member
    
    * calculate isTeamOwner in Members component
    
    After updating the users in the Members component via updateUsers(), the role of the current user needs to be recalculated dynamically as well. Therefore, we don't pass the role property from the php controller (which is static) and define the role property as a computed property in the Members component instead. Accordingly, the property isTeamOwner is recomputed as well.
    
    Co-authored-by: Marten <msigwart@users.noreply.github.com>
    
    * fix(datawrapper): hide actions for team owner (#323)
    
    * hide actions if team member is owner
    
    * show additional info for team owners
    
    * make role check type-strict
    
    Co-authored-by: David Kokkelink <david@datawrapper.de>
    
    Co-authored-by: Marten <msigwart@users.noreply.github.com>
    Co-authored-by: David Kokkelink <david@datawrapper.de>
    
    * show confirmation modal before changing the team owner
    
    * fix bug preventing team owner from changing ownership to someone else
    
    * remove icon from confirm modal
    
    * replace string placeholders with team name and user email
    
    * set custom line-height for modal title
    
    * hide team member actions for datawrapper admin who is also team owner
    
    Co-authored-by: elanals <19191012+elanals@users.noreply.github.com>
    
    * make member and team name bold
    
    Co-authored-by: elanals <19191012+elanals@users.noreply.github.com>
    
    * replace member placeholder with different property depending on which is present
    
    Co-authored-by: elanals <19191012+elanals@users.noreply.github.com>
    
    * fix formatting
    
    * change confirmation message if user is admin
    
    Co-authored-by: Marten <msigwart@users.noreply.github.com>
    Co-authored-by: David Kokkelink <david@datawrapper.de>
    Co-authored-by: elanals <19191012+elanals@users.noreply.github.com>

--- services/datawrapper/controller/team/settings.php
+++ services/datawrapper/controller/team/settings.php
@@ -151,7 +151,6 @@
 	            'defaultTheme' => $org->getDefaultTheme() ? $org->getDefaultTheme() : $getSystemDefaultTheme(),
 	            'pluginTabs' => $tabs ? $tabs : [],
 	            'settings' => $teamSettings,
-	            'role' => $org->hasUser($user) ? $org->getRole($user) : "",
 	            'hasThemeWithLogo' => $themeInfo["hasThemeWithLogo"]
 	        ]
 	    ];
