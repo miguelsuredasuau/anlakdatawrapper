c1fef6c1f17a596c2ab0179324ef85bb433b18c1
Gregor Aisch <gka@users.noreply.github.com>
2021-11-17 20:16:06 +0100

    chore(frontend): add new SettingsPageLayout

---
+++ services/frontend/src/views/_layout/SettingsPageLayout.svelte
@@ -0,0 +1,29 @@
+<script>
+    import MainLayout from '_layout/MainLayout.svelte';
+    import Menu from '_partials/Menu.svelte';
+
+    export let settingsPages;
+    export let title;
+
+    export let loadPage;
+</script>
+
+<style type="text/css">
+</style>
+
+<MainLayout title={`Settings - ${title}`}>
+    <section class="section">
+        <div class="container">
+            <div class="columns is-variable is-6">
+                <div class="column">
+                    <Menu groups={settingsPages} {loadPage} />
+                    <slot name="belowNav" />
+                </div>
+                <div class="column is-four-fifths content">
+                    <h1 class="title">Settings >> {title}</h1>
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </section>
+</MainLayout>
