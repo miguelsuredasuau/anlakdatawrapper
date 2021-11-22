c0ab3cee39890e0d9be827f263800c74e8ce3e5b
elanals <19191012+elanals@users.noreply.github.com>
2021-11-22 14:46:01 +0100

    fix(frontend): run inspect on correct port (#620)

--- services/frontend/docker/entrypoint.sh
+++ services/frontend/docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$DW_DEV_MODE" ] ; then
 	echo "Starting frontend and watching files for changes... "
-	exec nodemon --inspect=0.0.0.0:9229 "$@" --config nodemon.json
+	exec nodemon --inspect=0.0.0.0:9230 "$@" --config nodemon.json
 else
 	echo "Starting frontend"
 	exec node "$@"
