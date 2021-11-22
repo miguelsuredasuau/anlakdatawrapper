500e849eb5b862ce449a2e6e91fed6fcc0bec492
Gregor Aisch <gka@users.noreply.github.com>
2021-11-22 14:46:00 +0000

    chore(api/frontend): clean up nodemon.json files (#627)
    * chore(api/frontend): clean up nodemon.json files
    
    * chore(api): remove old nodemon config from package.json

--- services/api/docker/entrypoint.sh
+++ services/api/docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$DW_DEV_MODE" ] ; then
 	echo "Starting API and watching files for changes..."
-	exec nodemon --inspect=0.0.0.0:9229 "$@" --watch /datawrapper/code/services/api --watch /datawrapper/code/plugins --watch /etc/datawrapper/config.js
+	exec nodemon --inspect=0.0.0.0:9229 "$@" --config nodemon.json
 else
 	echo "Starting API"
 	exec node "$@"
