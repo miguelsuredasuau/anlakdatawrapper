b1d4e40ebb7482e410b59e9918ff8bb076262ac5
Marten Sigwart <marten@okamaja.de>
2022-01-03 11:33:23 +0100

    fix(river): fix pagination (#855)
    * install testing-library
    
    * add stores param to renderWithContext method
    
    * refactor: add query store as context
    
    Setting the query store as context allows easier testability of components that rely on the query store but that have default values of the query store set by parent components.
    By defining the query store as context we can set default values for the store using our utility Context.svelte component.
    
    * add basic tests for River page
    
    * add test for River pagination
    
    * fix: change loadVisualizations() signature
    
    By accepting query params directly as function arguments we make sure that the up-to-date values are passed to the http request
    
    * create build
    
    * rename context 'query' to 'plugins/river/query'
    
    Co-authored-by: Gregor Aisch <gka@users.noreply.github.com>
    
    * fix tests
    
    * execute all frontend tests
    
    * fix path
    
    Co-authored-by: Gregor Aisch <gka@users.noreply.github.com>

--- services/frontend/src/test-utils/index.mjs
+++ services/frontend/src/test-utils/index.mjs
@@ -6,49 +6,50 @@ export function setConfig(config) {
     configure(config);
 }
 
-export const defaultProps = {
-    stores: {
-        config: {
-            apiDomain: 'api.datawrapper.local',
-            frontendDomain: 'app.datawrapper.local',
-            imageDomain: 'charts.datawrapper.local/preview',
-            dev: 'true',
-            footerLinks: [],
-            languages: ['en-US'],
-            headerLinks: [],
-            stickyHeaderThreshold: 800
-        },
-        messages: getLocale(),
-        user: {
-            id: 2,
-            name: 'user@datawrapper.de',
-            email: 'user@datawrapper.de',
-            language: 'en-US',
-            isAdmin: false,
-            isGuest: false,
-            teams: [],
-            activeTeam: null,
-            isActivated: true
-        },
-        userData: {},
-        request: {
-            method: 'get',
-            // url: [URL],
-            path: '/archive',
-            params: {},
-            referrer: 'http://app.datawrapper.local/',
-            query: {}
-        }
+export const defaultStores = {
+    config: {
+        apiDomain: 'api.datawrapper.local',
+        frontendDomain: 'app.datawrapper.local',
+        imageDomain: 'charts.datawrapper.local/preview',
+        dev: 'true',
+        footerLinks: [],
+        languages: ['en-US'],
+        headerLinks: [],
+        stickyHeaderThreshold: 800
+    },
+    messages: getLocale(),
+    user: {
+        id: 2,
+        name: 'user@datawrapper.de',
+        email: 'user@datawrapper.de',
+        language: 'en-US',
+        isAdmin: false,
+        isGuest: false,
+        teams: [],
+        activeTeam: null,
+        isActivated: true
+    },
+    userData: {},
+    request: {
+        method: 'get',
+        // url: [URL],
+        path: '/archive',
+        params: {},
+        referrer: 'http://app.datawrapper.local/',
+        query: {}
     }
 };
 
-export async function renderWithContext(view, props) {
+export async function renderWithContext(view, props = {}, stores = {}) {
     // load locales again, in case the test loaded
     // additional plugin locales
-    defaultProps.stores.messages = getLocale();
+    defaultStores.messages = getLocale();
     return render(Context, {
         props: {
-            ...defaultProps,
+            stores: {
+                ...defaultStores,
+                ...stores
+            },
             view,
             ...props
         }
