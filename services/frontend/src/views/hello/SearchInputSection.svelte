e5e2c5c351caa29019f8b07d79ba13854b7f54ed
Simon Jockers <449739+sjockers@users.noreply.github.com>
2022-01-13 19:08:37 +0100

    fix(frontend): Update input handler for search ("hello" route) (#898)
    * fix(frontend): Update input handler for search (hello route)
    
    * Handle 'done' callback

--- services/frontend/src/views/hello/SearchInputSection.svelte
+++ services/frontend/src/views/hello/SearchInputSection.svelte
@@ -4,19 +4,19 @@
     let value = '';
     let results = 0;
 
-    function onSearch(event) {
+    function onSearch({ done }) {
         results = 0;
         setTimeout(() => {
             results = Math.round(Math.random() * 100);
-            event.detail.done();
+            done();
         }, 1000);
     }
 </script>
 
 <div class="section pl-0 pt-0">
-    <h3 id="search" class="title is-3">Set Password</h3>
+    <h3 id="search" class="title is-3">Search</h3>
     <div style="max-width: 400px">
-        <SearchInput {__} bind:value on:search={onSearch} />
+        <SearchInput {__} bind:value onInput={onSearch} />
     </div>
     {#if results}
         <p class="mt-2 has-text-grey">Found {results} results!</p>
