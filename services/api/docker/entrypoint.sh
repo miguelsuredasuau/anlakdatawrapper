f4dd369012dfa41768fb264a12316be48f6c0f4e
Jakub Valenta <58857807+jakubdatawrapper@users.noreply.github.com>
2022-01-19 15:38:39 +0100

    chore(services): Remove nodemon from api and frontend (#963)
    * api,frontend: Remove nodemon.json
    
    * api: Uninstall npm nodemon
    
    * frontend: Update package-lock
    
    * api,frontend: Start without nodemon in Docker
    
    * api,frontend: Don't install nodemon in Dockerfile
    
    * random-data: Uninstall unused npm nodemon

--- services/api/docker/entrypoint.sh
+++ services/api/docker/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-if [ "$DW_DEV_MODE" ] ; then
-	echo "Starting API and watching files for changes..."
-	exec nodemon --inspect=0.0.0.0:9229 "$@" --config nodemon.json
-else
-	echo "Starting API"
-	exec node "$@"
-fi
+echo "Starting API"
+exec node "$@"
--- services/frontend/docker/entrypoint.sh
+++ services/frontend/docker/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-if [ "$DW_DEV_MODE" ] ; then
-	echo "Starting frontend and watching files for changes... "
-	exec nodemon --inspect=0.0.0.0:9230 "$@" --config nodemon.json
-else
-	echo "Starting frontend"
-	exec node "$@"
-fi
+echo "Starting frontend"
+exec node "$@"
