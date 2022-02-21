b93bc0ceba589f7a8095a64445ff7cb528e9926a
Gregor Aisch <gka@users.noreply.github.com>
2022-02-21 10:39:08 +0000

    feat(admin-devtools): add query tester admin page (#1118)
    * feat(admin-devtools): add query tester admin page
    
    * only eval the options object

---
+++ services/frontend/src/views/_partials/controls/SelectInput.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    export let options; // [{ value, label }, ...] or [value, value, ...]
+    export let value;
+
+    const dispatch = createEventDispatcher();
+
+    $: options_ =
+        typeof options[0] === 'string' ? options.map(s => ({ value: s, label: s })) : options;
+</script>
+
+<div class="select">
+    <select bind:value on:change={() => dispatch('select', value)}>
+        {#each options_ as option}<option value={option.value}>{option.label}</option>{/each}
+    </select>
+</div>
