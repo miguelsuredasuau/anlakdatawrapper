bb99926461fe78b751649fa63bfeadc22222166b
Simon Jockers <449739+sjockers@users.noreply.github.com>
2022-07-25 11:54:27 +0200

    fix(api-v1): Remove legacy test (no longer valid after API change #1884) (#1917)

--- plugins/api-v1/src/api/routes/themes.test.js
+++ plugins/api-v1/src/api/routes/themes.test.js
@@ -1,7 +1,6 @@
 const test = require('ava');
 const {
     addThemeToTeam,
-    addUserToTeam,
     createTeam,
     createTeamWithUser,
     createThemes,
@@ -105,42 +104,6 @@ test('V1 GET /themes returns all themes for admins', async t => {
         t.assert(json.data.length >= expectedThemeIds.length);
         expectedThemeIds.forEach(theme => {
             t.assert(!json.data.find(el => el.id === theme.id));
-        });
-    } finally {
-        await destroy(themes, team, Object.values(userObj));
-    }
-});
-
-test('V1 GET /themes does not return default themes if team has restricted default themes', async t => {
-    let themes;
-    let team;
-    let userObj = {};
-    try {
-        userObj = await createUser(t.context.server, { role: 'editor' });
-        team = await createTeam({
-            settings: {
-                restrictDefaultThemes: true
-            }
-        });
-        await addUserToTeam(userObj.user, team);
-        themes = await createThemes([{ title: 'Theme 1' }, { title: 'Theme 2' }]);
-        await addThemeToTeam(themes[0], team);
-        await addThemeToTeam(themes[1], team);
-        const res = await t.context.server.inject({
-            method: 'GET',
-            url: `/v3/api-v1/themes`,
-            headers: {
-                ...t.context.headers,
-                Authorization: `Bearer ${userObj.token}`
-            }
-        });
-        const expectedThemeIds = [...themes.map(e => e.id)];
-        t.is(res.statusCode, 200);
-        const json = await res.result;
-        t.is(json.status, 'ok');
-        t.is(json.data.length, expectedThemeIds.length);
-        expectedThemeIds.forEach(id => {
-            t.assert(json.data.find(el => el.id === id));
         });
     } finally {
         await destroy(themes, team, Object.values(userObj));
