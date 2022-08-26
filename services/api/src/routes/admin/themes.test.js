ac50c1b56e884ab2b92d05722f4c5ff8b2f6f326
Gregor Aisch <gka@users.noreply.github.com>
2022-08-26 17:03:00 +0000

    chore(api): repair tests for admin/themes (#2065)
    * chore(api): repair tests for admin/themes
    
    * list of themes can only be as long as total - offset
    
    * theme length can't be negative

--- services/api/src/routes/admin/themes.test.js
+++ services/api/src/routes/admin/themes.test.js
@@ -9,15 +9,6 @@ const {
     createTeam
 } = require('../../../test/helpers/setup');
 
-async function findThemes(ids) {
-    const { Theme } = require('@datawrapper/orm/models');
-    return await Theme.findAll({
-        where: {
-            id: ids
-        }
-    });
-}
-
 const defaultHeaders = {
     cookie: 'crumb=abc',
     'X-CSRF-Token': 'abc',
@@ -30,28 +21,26 @@ test.before(async t => {
 });
 
 test('GET /admin/themes returns all existing themes for admins', async t => {
+    const { Theme } = require('@datawrapper/orm/models');
     let themes;
     let userObj;
     try {
         userObj = await createUser(t.context.server, { role: 'admin' });
-        const defaultThemeIds = t.context.config.general.defaultThemes;
         themes = await createThemes([{ title: 'Theme 1' }, { title: 'Theme 2' }]);
+        const limit = 100;
         const res = await t.context.server.inject({
             method: 'GET',
-            url: `/v3/admin/themes`,
+            url: `/v3/admin/themes?limit=${limit}`,
             headers: {
                 ...defaultHeaders,
                 Authorization: `Bearer ${userObj.token}`
             }
         });
-        const expectedThemeIds = [...defaultThemeIds, ...themes.map(e => e.id)];
+        const totalThemeCount = await Theme.count();
         t.is(res.statusCode, 200);
         const json = await res.result;
-        t.is(json.total, expectedThemeIds.length);
-        t.is(json.list.length, expectedThemeIds.length);
-        expectedThemeIds.forEach(id => {
-            t.assert(json.list.find(el => el.id === id));
-        });
+        t.is(json.total, totalThemeCount);
+        t.is(json.list.length, Math.min(limit, totalThemeCount));
     } finally {
         await destroy(themes, Object.values(userObj));
     }
@@ -96,6 +85,7 @@ test("GET /admin/themes returns an error if admin does not have scope 'theme:rea
 });
 
 test('GET /admin/themes returns themes correctly paginated', async t => {
+    const { Theme } = require('@datawrapper/orm/models');
     let createdThemes;
     let team;
     let userObj = {};
@@ -108,12 +98,9 @@ test('GET /admin/themes returns themes correctly paginated', async t => {
         });
         const alphabet = Array.from(Array(26).keys()).map(n => String.fromCharCode(65 + n));
         createdThemes = await createThemes(alphabet.map(letter => ({ title: `Theme ${letter}` })));
-        const defaultThemeIds = t.context.config.general.defaultThemes;
-        const expectedThemes = [...createdThemes, ...(await findThemes(defaultThemeIds))].sort(
-            (a, b) => a.title.localeCompare(b.title)
-        );
+        const totalThemeCount = await Theme.count();
 
-        const testPagination = async (offset, limit, expectedThemes) => {
+        const testPagination = async (offset, limit) => {
             const res = await t.context.server.inject({
                 method: 'GET',
                 url: `/v3/admin/themes?offset=${offset}&limit=${limit}`,
@@ -122,19 +109,15 @@ test('GET /admin/themes returns themes correctly paginated', async t => {
                     Authorization: `Bearer ${userObj.token}`
                 }
             });
-            const expectedList = expectedThemes.slice(offset, offset + limit);
             t.is(res.statusCode, 200);
             const json = await res.result;
-            t.is(json.total, expectedThemes.length);
-            t.is(json.list.length, expectedList.length);
-            expectedList.forEach(theme => {
-                t.assert(json.list.find(el => el.id === theme.id));
-            });
+            t.is(json.total, totalThemeCount);
+            t.is(json.list.length, Math.max(0, Math.min(limit, totalThemeCount - offset)));
         };
-        await testPagination(0, 10, expectedThemes);
-        await testPagination(10, 10, expectedThemes);
-        await testPagination(20, 10, expectedThemes);
-        await testPagination(30, 10, expectedThemes);
+        await testPagination(0, 10);
+        await testPagination(10, 10);
+        await testPagination(20, 10);
+        await testPagination(30, 10);
     } finally {
         await destroy(createdThemes, team, Object.values(userObj));
     }
